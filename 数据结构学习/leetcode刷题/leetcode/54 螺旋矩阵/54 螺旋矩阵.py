616c984a2406b682279ba37ac81adc5236242a14
danjuan5hao <danjuan5hao@hotmail.com>
2021-03-17 13:19:53 +0800

    leetcode

--- 数据结构学习/leetcode刷题/leetcode/54 螺旋矩阵/54 螺旋矩阵.py
+++ 数据结构学习/leetcode刷题/leetcode/54 螺旋矩阵/54 螺旋矩阵.py
@@ -4,29 +4,53 @@ from typing import List
 class Solution:
 
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        
-        # def check_next(direction, i, j):
-        #     if direction == "left":
-        #         next_i = i+1
-        #         next_j = j
-        #     elif direction == "right":
-        #         next_i = i-1
-        #         next_j = j
-        #     elif direction == "down":
-        #         next_i = i-1
-        #         next_j = j 
-        #     elif direction == "up":
-        #         next_i = i+1
-        #         next_j = j
+        rst = []
+        i = 0
+        j = -1
+        row_step = len(matrix[0])
+        col_step = len(matrix)-1
 
-        pass 
+        direction = "right"
+
+        while True:                     
+            if direction ==  "right" and row_step !=0:
+                for _ in range(row_step):
+                    j += 1
+                    rst.append(matrix[i][j])
+                    
+                direction = "down"
+                row_step -= 1
+            elif direction == "down" and col_step !=0:
+                for _ in range(col_step):
+                    i += 1
+                    rst.append(matrix[i][j])
+                direction = "left"
+                col_step -= 1
+            elif direction == "left" and row_step !=0:
+                for _ in range(row_step):
+                    j -= 1
+                    rst.append(matrix[i][j])
+                direction = "up"
+                row_step -= 1
+            elif direction == "up" and col_step !=0:
+                for _ in range(col_step):
+                    i -= 1
+                    rst.append(matrix[i][j])
+                direction = "right"
+                col_step -= 1
+            else:
+                break
+
+        return rst
 
 
 if __name__ == "__main__":
     test_1  = [[1,2,3],[4,5,6],[7,8,9]]
     test_1_rst = [1,2,3,6,9,8,7,4,5]
-    assert Solution().spiralOrder(test_1) == test_1_rst 
+    # assert Solution().spiralOrder(test_1) == test_1_rst 
+    print(Solution().spiralOrder(test_1))
 
     test_2 = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
     test_2_rst = [1,2,3,4,8,12,11,10,9,5,6,7]
-    assert Solution().spiralOrder(test_2) == test_2_rst 
+    # assert Solution().spiralOrder(test_2) == test_2_rst 
+    print(Solution().spiralOrder(test_2))
